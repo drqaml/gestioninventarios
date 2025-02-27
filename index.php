bf98bdfbfef264de513cd868adf89975459e240d
Juan <felixcardozo30@gmail.com>
2025-02-27 10:28:06 -0500

    Agregamos archivos iniciales del login y register

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Js company</title>
+    <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<div class="container-all">
+    <div class="ctn.form">
+        <img src="" alt="" class="logo">
+        <h1 class="title">Iniciar sesión</h1>
+
+        <form action="">
+            <label for="">Correo electrónico</label>
+            <input type="text" name="email" id="" required>
+
+            <label for="">Contraseña</label>
+            <input type="password" name="password" id="" required>
+
+            <input type="submit" value="Iniciar sesión">
+        </form>
+
+        <span class="text-footer">¿Aún no tienes una cuenta? <a href="#">Regístrate</a></span>
+    </div>
+    <div class="ctn text">
+        <div class="capa"></div>
+        <h1 class="title-description">Bienvenido a Js company</h1>
+        <p class="text-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, doloremque.</p>
+    </div>
+
+</div>
+
+</body>
+</html>
