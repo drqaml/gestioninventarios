23c820373f4d9edebef51377cf1fe0d5d63c09c5
Juan <felixcardozo30@gmail.com>
2025-02-27 12:49:46 -0500

    Mejoras esteticas y pantalla de bienvenida

---
+++ bienvenida.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bienvenida</title>
+    <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+<div class="ctn-welcome">
+    <img src="assets/Logo.jpg" class="logo-welcome" alt="">
+    <h1 class="title-welcome">Bienvenido a Js company</h1>
+    <a href="#" class="close-session">Cerrar sesión</a>
+</div>
+
+</body>
+</html>
